0ec458565bcfa2ae7b9046f6de743f095b99f6c8
microslaw <milosz.grunwald@gmail.com>
2025-04-11 11:45:10 +0200

    add BreezeApp class

--- breeze.py
+++ breeze.py
@@ -1,12 +1,3 @@
 # TODO: turn this file into __init__
-from backend import Controller
-from backend import Repository
-from backend import Processor
-
-repository = Repository()
-
-
-def start():
-    processor = Processor(repository)
-    controller = Controller(repository, processor)
-    controller.run(debug=True)
+from backend import BreezeApp
+from backend import NodeType

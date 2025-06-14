9cb6596dd466f6f1650fe45d98c7124de712a52b
mszablewski <mszablewski2004@gmail.com>
2025-06-14 12:18:37 +0200

    init kwargs service

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from breeze import BreezeApp, NodeType
 br = BreezeApp()
 
 # TODO remove once nodeSet imports are implemented
-import backend.prefabs.testing.processing
+import backend.prefabs.testing.kwargs
 
-br.repository.load_workflow("backend/tests/workflows/processing")
+br.repository.load_workflow("backend/tests/workflows/kwargs")
 
 br.start()

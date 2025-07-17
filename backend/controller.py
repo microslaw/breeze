67a2e1876c2e860610bd34002e46cdd4e060616f
microslaw <milosz.grunwald@gmail.com>
2025-07-17 21:46:53 +0200

    feat: add simple blob endpoint

--- backend/controller.py
+++ backend/controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, testing
+from flask import Flask, request, testing, send_file
 from flask_cors import CORS
 from backend.datatypes import NodeInstance, NodeLink
 from backend.repository import Repository
@@ -166,6 +166,16 @@ class Controller:
                 format_from_input(request.data, arg_type), node_id, kwarg_name
             )
             return "OK", 200
+
+        @self.flask_server.route("/blob/", methods=["GET"])
+        def get_blob():
+            return send_file("tests/workflows/etiWalpaper.png", "png"), 200
+
+        @self.flask_server.route("/blob/raw", methods=["GET"])
+        def get_blob_raw():
+            with open("backend/tests/workflows/etiWalpaper.png", "rb") as wfile:
+                data = wfile.read()
+            return data, 200
 
     def test_client(self, **kwargs: Any) -> testing.FlaskClient:
         return self.flask_server.test_client(**kwargs)

eed62652d819bb2d7c02a33814a40df0cceab410
microslaw <milosz.grunwald@gmail.com>
2025-08-28 21:32:26 +0200

    refactor: restructure docs configuration

--- backend/docs/source/conf.py
+++ backend/docs/source/conf.py
@@ -1,3 +1,5 @@
+import sys
+import os
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,23 +8,29 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'breeze'
-copyright = '2025, microslaw, mszablewski'
-author = 'microslaw, mszablewski'
-release = '0.1'
+x = os.path.abspath(os.path.join("..", "..", ".."))
+print(x)
+sys.path.insert(0, x)
+
+project = "breeze"
+copyright = "2025, microslaw, mszablewski"
+author = "microslaw, mszablewski"
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_automodapi.automodapi",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]

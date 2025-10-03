ee2944da3385ab45cb20526af430ba7762cae8e0
mszablewski <mszablewski2004@gmail.com>
2025-10-03 19:28:21 +0200

    besic wirking sse

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,171 +19,187 @@ import { getProcessingQueue } from "./services/processingApiService";
 import { BLOCK_DEFAULT_COLOUR } from "./constants/ui";
 
 function App() {
-  const [blocks, setBlocks] = useState<BlockI[]>([]);
-  const [links, setLinks] = useState<LinkI[]>([]);
-  const [processingQueue, setProcessingQueue] = useState<number[]>([]);
+  // const [blocks, setBlocks] = useState<BlockI[]>([]);
+  // const [links, setLinks] = useState<LinkI[]>([]);
+  // const [processingQueue, setProcessingQueue] = useState<number[]>([]);
+
+  // useEffect(() => {
+  //   const fetchAppState = async () => {
+  //     const blocks = await getAllNodes();
+  //     const links = await getAllLinks();
+  //     setBlocks(blocks);
+  //     setLinks(links);
+  //     assignLinksPositionByBlocksPosition(blocks, links);
+  //   };
+  //   fetchAppState();
+
+  //   let interval: number | null = null;
+  //   interval = setInterval(updateProcessingQueue, 1000);
+  // }, []);
 
   useEffect(() => {
-    const fetchAppState = async () => {
-      const blocks = await getAllNodes();
-      const links = await getAllLinks();
-      setBlocks(blocks);
-      setLinks(links);
-      assignLinksPositionByBlocksPosition(blocks, links);
+    const es = new EventSource("http://127.0.0.1:5000/stream");
+    es.onopen = (e) => console.log(">>> Connection opened!", e);
+    es.onerror = (e) => console.log("ERROR!", e);
+    es.onmessage = (e) => {
+      try {
+        const data = JSON.parse(e.data);
+        console.log(">>>", data);
+      } catch {
+        console.log(">>>", e.data);
+      }
     };
-    fetchAppState();
 
-    let interval: number | null = null;
-    interval = setInterval(updateProcessingQueue, 1000);
+    return () => es.close();
   }, []);
 
-  const [isBlockModalDetailsVisible, setIsBlockModalDetailsVisible] =
-    useState<boolean>(false);
+  //   const [isBlockModalDetailsVisible, setIsBlockModalDetailsVisible] =
+  //     useState<boolean>(false);
 
-  const [isBlockModalCreateVisible, setIsBlockModalCreateVisible] =
-    useState<boolean>(false);
+  //   const [isBlockModalCreateVisible, setIsBlockModalCreateVisible] =
+  //     useState<boolean>(false);
 
-  const [isLinkModalDeatilsVisible, setIsLinkModalDeatilsVisible] =
-    useState<boolean>(false);
+  //   const [isLinkModalDeatilsVisible, setIsLinkModalDeatilsVisible] =
+  //     useState<boolean>(false);
 
-  const [isLinkModalCreateVisible, setIsLinkModalCreateVisible] =
-    useState<boolean>(false);
+  //   const [isLinkModalCreateVisible, setIsLinkModalCreateVisible] =
+  //     useState<boolean>(false);
 
-  const [isQueueModalDetailsVisible, setIsQueueModalDetailsVisible] =
-    useState<boolean>(false);
+  //   const [isQueueModalDetailsVisible, setIsQueueModalDetailsVisible] =
+  //     useState<boolean>(false);
 
-  const [selectedBlock, setSelectedBlock] = useState<BlockI>({
-    id: -1,
-    name: "",
-    type: "default",
-    x: 0,
-    y: 0,
-    isDragging: false,
-    isSelected: false,
-    isQueued: false,
-    kwargs: [],
-    colour: BLOCK_DEFAULT_COLOUR,
-  });
+  //   const [selectedBlock, setSelectedBlock] = useState<BlockI>({
+  //     id: -1,
+  //     name: "",
+  //     type: "default",
+  //     x: 0,
+  //     y: 0,
+  //     isDragging: false,
+  //     isSelected: false,
+  //     isQueued: false,
+  //     kwargs: [],
+  //     colour: BLOCK_DEFAULT_COLOUR,
+  //   });
 
-  const [selectedLink, setSelectedLink] = useState<LinkI>({
-    id: -1,
-    destinationNodeId: 0,
-    destinationNodeInput: "",
-    originNodeId: 0,
-    originNodeOutput: "",
-    startX: 0,
-    startY: 0,
-    endX: 0,
-    endY: 0,
-  });
+  //   const [selectedLink, setSelectedLink] = useState<LinkI>({
+  //     id: -1,
+  //     destinationNodeId: 0,
+  //     destinationNodeInput: "",
+  //     originNodeId: 0,
+  //     originNodeOutput: "",
+  //     startX: 0,
+  //     startY: 0,
+  //     endX: 0,
+  //     endY: 0,
+  //   });
 
-  const handleBlockDoubleClick = (block: BlockI) => {
-    setSelectedBlock(block);
-    setIsBlockModalDetailsVisible(true);
-  };
+  //   const handleBlockDoubleClick = (block: BlockI) => {
+  //     setSelectedBlock(block);
+  //     setIsBlockModalDetailsVisible(true);
+  //   };
 
-  const handleLinkDoubleClick = (link: LinkI) => {
-    setSelectedLink(link);
-    setIsLinkModalDeatilsVisible(true);
-  };
+  //   const handleLinkDoubleClick = (link: LinkI) => {
+  //     setSelectedLink(link);
+  //     setIsLinkModalDeatilsVisible(true);
+  //   };
 
-  const handleCloseBlockDetails = () => {
-    setIsBlockModalDetailsVisible(false);
-  };
+  //   const handleCloseBlockDetails = () => {
+  //     setIsBlockModalDetailsVisible(false);
+  //   };
 
-  const handleCloseLinkDetails = () => {
-    setIsLinkModalDeatilsVisible(false);
-  };
+  //   const handleCloseLinkDetails = () => {
+  //     setIsLinkModalDeatilsVisible(false);
+  //   };
 
-  const handleDeleteBlock = (blockId: number) => {
-    deleteNodeById(blockId);
-    setBlocks((prevBlocks) =>
-      prevBlocks.filter((block) => block.id !== selectedBlock.id)
-    );
-    setLinks((prevLinks) =>
-      prevLinks.filter(
-        (link) =>
-          link.originNodeId !== selectedBlock.id &&
-          link.destinationNodeId !== selectedBlock.id
-      )
-    );
-    handleCloseBlockDetails();
-  };
+  //   const handleDeleteBlock = (blockId: number) => {
+  //     deleteNodeById(blockId);
+  //     setBlocks((prevBlocks) =>
+  //       prevBlocks.filter((block) => block.id !== selectedBlock.id)
+  //     );
+  //     setLinks((prevLinks) =>
+  //       prevLinks.filter(
+  //         (link) =>
+  //           link.originNodeId !== selectedBlock.id &&
+  //           link.destinationNodeId !== selectedBlock.id
+  //       )
+  //     );
+  //     handleCloseBlockDetails();
+  //   };
 
-  const handleDeleteLink = (linkId: number) => {
-    deleteLinkById(linkId);
-    setLinks((prevLinks) => prevLinks.filter((link) => link.id !== linkId));
-    handleCloseLinkDetails();
-  };
+  //   const handleDeleteLink = (linkId: number) => {
+  //     deleteLinkById(linkId);
+  //     setLinks((prevLinks) => prevLinks.filter((link) => link.id !== linkId));
+  //     handleCloseLinkDetails();
+  //   };
 
-  function updateProcessingQueue() {
-    getProcessingQueue().then((queue) => {
-      setProcessingQueue(queue);
-      setBlocks((prevBlocks) => {
-        const updatedBlocks = [...prevBlocks];
-        updatedBlocks.forEach((block) => {
-          queue.find((item) => block.id === item)
-            ? (block.isQueued = true)
-            : (block.isQueued = false);
-        });
-        return updatedBlocks;
-      });
-    });
-  }
+  //   function updateProcessingQueue() {
+  //     getProcessingQueue().then((queue) => {
+  //       setProcessingQueue(queue);
+  //       setBlocks((prevBlocks) => {
+  //         const updatedBlocks = [...prevBlocks];
+  //         updatedBlocks.forEach((block) => {
+  //           queue.find((item) => block.id === item)
+  //             ? (block.isQueued = true)
+  //             : (block.isQueued = false);
+  //         });
+  //         return updatedBlocks;
+  //       });
+  //     });
+  //   }
 
-  return (
-    <div>
-      <Menu
-        processingQueue={processingQueue}
-        blocks={blocks}
-        setIsBlockModalCreateVisible={setIsBlockModalCreateVisible}
-        setIsQueueModalDetailsVisible={setIsQueueModalDetailsVisible}
-      />
-      {/* TODO add component wrapping MainStage and modals associated with its elements*/}
-      <MainStage
-        blocks={blocks}
-        setBlocks={setBlocks}
-        links={links}
-        setLinks={setLinks}
-        setSelectedLink={setSelectedLink}
-        setIsBlockModalCreateVisible={setIsBlockModalCreateVisible}
-        setIsLinkModalCreateVisible={setIsLinkModalCreateVisible}
-        handleBlockDoubleClick={(block) => handleBlockDoubleClick(block)}
-        handleLinkDoubleClick={(link) => handleLinkDoubleClick(link)}
-      />
-      <BlockModalDetails
-        block={selectedBlock}
-        setBlock={setSelectedBlock}
-        show={isBlockModalDetailsVisible}
-        handleClose={() => handleCloseBlockDetails()}
-        handleDelete={(blockId) => handleDeleteBlock(blockId)}
-      />
-      <BlockModalCreate
-        show={isBlockModalCreateVisible}
-        handleClose={() => setIsBlockModalCreateVisible(false)}
-        blocks={blocks}
-        setBlocks={setBlocks}
-      ></BlockModalCreate>
-      <LinkModalDetails
-        link={selectedLink}
-        show={isLinkModalDeatilsVisible}
-        handleClose={() => handleCloseLinkDetails()}
-        handleDelete={(linkId) => handleDeleteLink(linkId)}
-      />
-      <LinkModalCreate
-        show={isLinkModalCreateVisible}
-        blocks={blocks}
-        link={selectedLink}
-        setLinks={setLinks}
-        handleClose={() => setIsLinkModalCreateVisible(false)}
-      />
-      <QueueModalDetails
-        show={isQueueModalDetailsVisible}
-        handleClose={() => setIsQueueModalDetailsVisible(false)}
-        processingQueue={processingQueue}
-      ></QueueModalDetails>
-    </div>
-  );
+  //   return (
+  //     <div>
+  //       <Menu
+  //         processingQueue={processingQueue}
+  //         blocks={blocks}
+  //         setIsBlockModalCreateVisible={setIsBlockModalCreateVisible}
+  //         setIsQueueModalDetailsVisible={setIsQueueModalDetailsVisible}
+  //       />
+  //       {/* TODO add component wrapping MainStage and modals associated with its elements*/}
+  //       <MainStage
+  //         blocks={blocks}
+  //         setBlocks={setBlocks}
+  //         links={links}
+  //         setLinks={setLinks}
+  //         setSelectedLink={setSelectedLink}
+  //         setIsBlockModalCreateVisible={setIsBlockModalCreateVisible}
+  //         setIsLinkModalCreateVisible={setIsLinkModalCreateVisible}
+  //         handleBlockDoubleClick={(block) => handleBlockDoubleClick(block)}
+  //         handleLinkDoubleClick={(link) => handleLinkDoubleClick(link)}
+  //       />
+  //       <BlockModalDetails
+  //         block={selectedBlock}
+  //         setBlock={setSelectedBlock}
+  //         show={isBlockModalDetailsVisible}
+  //         handleClose={() => handleCloseBlockDetails()}
+  //         handleDelete={(blockId) => handleDeleteBlock(blockId)}
+  //       />
+  //       <BlockModalCreate
+  //         show={isBlockModalCreateVisible}
+  //         handleClose={() => setIsBlockModalCreateVisible(false)}
+  //         blocks={blocks}
+  //         setBlocks={setBlocks}
+  //       ></BlockModalCreate>
+  //       <LinkModalDetails
+  //         link={selectedLink}
+  //         show={isLinkModalDeatilsVisible}
+  //         handleClose={() => handleCloseLinkDetails()}
+  //         handleDelete={(linkId) => handleDeleteLink(linkId)}
+  //       />
+  //       <LinkModalCreate
+  //         show={isLinkModalCreateVisible}
+  //         blocks={blocks}
+  //         link={selectedLink}
+  //         setLinks={setLinks}
+  //         handleClose={() => setIsLinkModalCreateVisible(false)}
+  //       />
+  //       <QueueModalDetails
+  //         show={isQueueModalDetailsVisible}
+  //         handleClose={() => setIsQueueModalDetailsVisible(false)}
+  //         processingQueue={processingQueue}
+  //       ></QueueModalDetails>
+  //     </div>
+  //   );
 }
 
 export default App;
--- tmp.py
+++ tmp.py
@@ -8,47 +8,22 @@
 
 
 from flask import Flask
-from flask_queue_sse import ServerSentEvents
+from flask_cors import CORS
 import time
-from threading import Thread
 
 app = Flask(__name__)
-sse: ServerSentEvents = None
+CORS(app, origins=["http://localhost:5173"])
 
 
-def spam_requests():
-    for i in range(1000):
-        time.sleep(1)
-        if sse:
-            sse.send({"msg": f"message {i}"})
-        else:
-            print(f"didn't send message {i}")
+@app.route("/stream")
+def stream():
+    def get_data():
+        while True:
+            time.sleep(1)
+            yield f"data: The time is {time.ctime()}\n\n"
 
+    return app.response_class(get_data(), mimetype="text/event-stream")
 
-@app.route("/end")
-def end():
-    global sse
-
-    if sse:
-        sse.send(event="end")
-        sse = None
-    else:
-        return "NO CONNECTION"
-
-    return "FINISHED"
-
-
-@app.route("/subscribe")
-def subscribe():
-    global sse
-
-    # create a new server sent events channel
-    sse = ServerSentEvents()
-
-    return sse.response()
 
 if __name__ == "__main__":
-    thread = Thread(target = spam_requests)
-    thread.start()
     app.run()
-    thread.join()

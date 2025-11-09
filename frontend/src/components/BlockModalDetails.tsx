14808e528e88161bfaec53871db64ff7df864d54
mszablewski <mszablewski2004@gmail.com>
2025-11-09 12:20:32 +0100

    fix

--- frontend/src/components/BlockModalDetails.tsx
+++ frontend/src/components/BlockModalDetails.tsx
@@ -189,10 +189,10 @@ const BlockModalDetails = ({
     if (processingResultMetadata.frontend_type === FrontendTypeEnum.HtmlDiv) {
       return (
         <Card.Body>
-            <div
-              className={styles.processingResult}
-              dangerouslySetInnerHTML={{ __html: processingResult }}
-            />
+          <div
+            className={styles.processingResult}
+            dangerouslySetInnerHTML={{ __html: processingResult }}
+          />
           <Card.Text className={styles.processedAt}>
             Processed at:{" "}
             <span className={styles.processedAtDate}>
